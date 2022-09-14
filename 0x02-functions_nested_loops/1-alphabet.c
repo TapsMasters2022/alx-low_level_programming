188771f2e83a4d0ac1648d036ff1fbf6706b8b09
TapsMasters2022 <tapsmaenz@gmail.com>
2022-09-14 07:52:19 -0700

    1. albhabet lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print _alphabet  - function to print abc
+ *
+ * Return: 0
+ * */
+voidd print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ctt)
+	{
+		_putchar(c);	
+	}
+_putchar('\n');
+}
